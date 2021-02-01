22bbc3ae1e8498e6c930f84de36a2230a58cafcc
Matthew Budiman <matthewbudiman@outlook.com>
2021-02-01 16:45:28 -0600

    Updated docker config

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConn = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/sdcdb', {
+    await mongoose.connect('mongodb://sdcdb:27017/sdcdb', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
